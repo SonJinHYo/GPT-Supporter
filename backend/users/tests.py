13e0f33b88ece671dbc1357df98642767ef2c5ae
SonJinHYo <wlsgy95@naver.com>
2023-08-12 17:16:11 +0900

    test : 회원가입 테스트코드 작성 정상 데이터, 필드누락, 패스워드 길이 테스트

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from .models import User
 
-# Create your tests here.
+
+class SignUpAPITest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse("signup")  # "signup" 이름의 URL을 가져옴
+
+    def test_signup_valid_data(self):
+        data = {
+            "username": "testuser",
+            "email": "test@example.com",
+            "password": "testpassword123",
+        }
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(
+            User.objects.get().username,
+            "testuser",
+        )
+
+    def test_signup_missing_fields(self):
+        data = {
+            "username": "testuser",
+            "password": "testpassword123",
+        }
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_signup_invalid_email(self):
+        data = {
+            "username": "testuser",
+            "email": "invalid_email",
+            "password": "testpassword123",
+        }
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST,
+        )
+        self.assertEqual(User.objects.count(), 0)
