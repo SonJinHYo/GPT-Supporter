fa314eed4bc51fd763ae094693cf2576901660bf
SonJinHYo <wlsgy95@naver.com>
2023-08-12 16:31:18 +0900

    backend-feature : SystemInfo 모델 추가 chatGPT에게 전달할 유저 정보 묶음 language major understanding_level only_use_reference_data created_at updated_at

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinLengthValidator
+from django.core.validators import (
+    MinLengthValidator,
+    MinValueValidator,
+    MaxValueValidator,
+)
 
 
 class User(AbstractUser):
@@ -13,5 +17,24 @@ class User(AbstractUser):
         validators=[MinLengthValidator(8)],
     )
     using_token = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class SystemInfo(models.Model):
+    LANGUAGES = [
+        ("en", "English"),
+        ("ko", "Korean"),
+    ]
+
+    language = models.CharField(max_length=2, choices=LANGUAGES)
+    major = models.CharField(max_length=20)
+    understanding_level = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(0, message="대학교 기준입니다. 1~5학년을 선택해주세요"),
+            MaxValueValidator(5, "대학교 기준입니다. 1~5학년을 선택해주세요"),
+        ]
+    )
+    only_use_reference_data = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
