31d788d5f50cbe0f308a3ec912d6e04060c10f19
SonJinHYo <wlsgy95@naver.com>
2023-08-12 18:46:42 +0900

    backend-feature: Chatroom, Message 모델 수정

--- backend/chatrooms/models.py
+++ backend/chatrooms/models.py
@@ -2,12 +2,22 @@ from django.db import models
 
 
 class ChatRoom(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="chatrooms",
+    )
     name = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Message(models.Model):
+    chatroom = models.ForeignKey(
+        "chatrooms.ChatRoom",
+        on_delete=models.CASCADE,
+        related_name="messages",
+    )
     sender = models.CharField(max_length=20)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
