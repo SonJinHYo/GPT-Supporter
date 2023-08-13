8568954e8462b649f956d5e2a1f32f3001717e57
SonJinHYo <wlsgy95@naver.com>
2023-08-13 15:47:31 +0900

    backend-test: 참조 책 리스트 테스트코드 작성. pagination까지 확인

--- backend/gpt_sys_infos/tests.py
+++ backend/gpt_sys_infos/tests.py
@@ -20,10 +20,10 @@ class CreateRefBookTestCase(APITestCase):
         self.user = User.objects.create_user(
             username="testuser", password="testpassword"
         )
-        self.url = reverse("create-refbook")  # 해당 URL 생성
+        self.url = reverse("create-refbook")
 
     def test_create_ref_book(self):
-        self.client.force_authenticate(user=self.user)  # 인증된 사용자로 설정
+        self.client.force_authenticate(user=self.user)
         data = {"author": "Test Author", "title": "Test Title"}
         response = self.client.post(self.url, data)
 
@@ -36,12 +36,50 @@ class CreateRefBookTestCase(APITestCase):
         self.client.force_authenticate(user=self.user)
         invalid_data = {
             "author": "Test Author",
-            # "title": "Test Title"  # title 필드가 누락된 무효한 데이터
+            # "title": "Test Title",
         }
         response = self.client.post(self.url, invalid_data)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(RefBook.objects.count(), 0)  # 무효한 데이터로 인해 생성되지 않아야 함
+
+
+class RefBooksListTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+        self.client.force_authenticate(user=self.user)
+
+        self.ref_book_1 = RefBook.objects.create(author="Author 1", title="Title 1")
+        self.ref_book_2 = RefBook.objects.create(author="Author 2", title="Title 2")
+
+        self.url = reverse("refbook")
+
+        self.page_size = settings.REST_FRAMEWORK["PAGE_SIZE"]
+        self.obj_count = 26
+        self.last_page = self.obj_count // self.page_size + 1
+        self.rem_page = self.obj_count % self.page_size
+
+        for i in range(3, self.obj_count + 1):
+            RefBook.objects.create(author=f"Author {i}", title=f"Title {i}")
+
+    def test_list_ref_books(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]["author"], self.ref_book_1.author)
+        self.assertEqual(response.data[0]["title"], self.ref_book_1.title)
+        self.assertEqual(response.data[1]["author"], self.ref_book_2.author)
+        self.assertEqual(response.data[1]["title"], self.ref_book_2.title)
+
+    def test_pagination_ref_books(self):
+        for page in range(1, self.last_page + 1):
+            response = self.client.get(f"{self.url}" + f"?page={page}")
+            if page != self.last_page:
+                self.assertEqual(len(response.data), self.page_size)
+            else:
+                self.assertEqual(len(response.data), self.rem_page)
 
 
 # class CreateSysInfoTest(APITestCase):
