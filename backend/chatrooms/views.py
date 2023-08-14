6c12ea322043bc449889a11e8689a04f7b42738f
SonJinHYo <wlsgy95@naver.com>
2023-08-14 14:18:15 +0900

    backend-feature: 채팅방 리스트 조회 코드 작성

--- backend/chatrooms/views.py
+++ backend/chatrooms/views.py
@@ -10,6 +10,7 @@ from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from .models import ChatRoom
 from users.models import User
 
 
@@ -41,5 +42,15 @@ class CreateChatRoom(APIView):
 
 
 class ChatRoomsList(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        pass
+        chatrooms = ChatRoom.objects.all()
+        serializer = serializers.CreateChatRoomSerializer(
+            chatrooms,
+            many=True,
+        )
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
