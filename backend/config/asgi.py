d151a98803aa960a1a49f7c701470926654672f5
SonJinHYo <wlsgy95@naver.com>
2023-08-15 13:42:58 +0900

    backend-chore: asgi 구동으로 변경 및 관련 세팅파일 수정, 라이브러리 추가

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chatrooms.urls
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter(chatrooms.urls.urlpatterns),
+    }
+)
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 SYSTEM_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -84,6 +85,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 
 # Database
