0066cb9df33af12f929ef1e465d58e914e9573c1
SonJinHYo <wlsgy95@naver.com>
2023-08-19 11:36:57 +0900

    backend-chore: CORS 허용 추가

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,6 +51,7 @@ CUSTOM_APPS = [
 
 THRID_PARTY_APPS = [
     "rest_framework",
+    "corsheaders",
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS + THRID_PARTY_APPS
@@ -64,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -151,3 +153,5 @@ REST_FRAMEWORK = {
 }
 
 CHATROOMS_MAX_SIZE = 10
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
