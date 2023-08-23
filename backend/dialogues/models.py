68d772aba1d5d4f8aa61ba07214c17d5cb24a286
SonJinHYo <wlsgy95@naver.com>
2023-08-23 12:26:15 +0900

    backend-feature: dialogue 모델 추가

--- backend/dialogues/models.py
+++ backend/dialogues/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Dialogue(models.Model):
+    system_info = models.ForeignKey(
+        "gpt_sys_info.SystemInfo",
+        on_delete=models.SET_NULL,
+        related_name="dialogue",
+    )
+
+
+class DialogueContent(models.Model):
+    dialogue = models.ForeignKey(
+        "dialogues.Dialogue",
+        on_delete=models.CASCADE,
+        related_name="contents",
+    )
+    number = models.PositiveSmallIntegerField()
+    text = models.TextField()
