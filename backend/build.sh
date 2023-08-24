ef6d0037c139e80522ba4f176b38188ce361e537
SonJinHYo <wlsgy95@naver.com>
2023-08-24 17:39:30 +0900

    backend-chore : 서버를 gunicorn으로 변경

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,8 @@
 # [local]
 python3 manage.py makemigrations --settings=config.settings.local
 python3 manage.py migrate --settings=config.settings.local
-daphne -b 0.0.0.0 -p 8000 config.asgi:application
+gunicorn -c gunicorn_conf.py config.wsgi:application
+# daphne -b 0.0.0.0 -p 8000 config.asgi:application
 # python3 manage.py runserver 0.0.0.0:8000
 
 # # product
