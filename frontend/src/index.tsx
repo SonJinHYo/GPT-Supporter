db2b8b63daaa72ab07a2d82998f664872e65f890
SonJinHYo <wlsgy95@naver.com>
2023-08-25 12:15:09 +0900

    frontend-style: 항시 다크모드 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,7 +15,8 @@ const client = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RouterProvider router={router} />
       </ChakraProvider>
     </QueryClientProvider>
