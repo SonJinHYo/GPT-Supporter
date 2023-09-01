b10fcd7859f391206a43e652e1f692a932d0d1cc
SonJinHYo <wlsgy95@naver.com>
2023-09-02 00:49:37 +0900

    backend-feature: public_scripts 모델 추가 PublicScript 모델 추가 - 공용 스크립트 모델.  - name (char) : 공용 스크립트 이름  - description (text) : 공용 스크립트 설명 Script 모델 추가 - 공용 스크립트의 스크립트 내용  - public_script (foreignKey) : PublicScript 모델 FK  - number (positiveSmallInteger) : 스크립트 순번  - text (text) : 스크립트 내용

--- backend/public_scripts/models.py
+++ backend/public_scripts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class PublicScript(models.Model):
+    """공용 스크립트 모델"""
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="공용 스크립트 이름",
+    )
+    description = models.TextField(
+        verbose_name="공용 스크립트 설명서",
+        help_text="스크립트를 ChatGPT에게 전했을 때 효과, 이후 사용법 등 설명",
+    )
+
+
+class Script(models.Model):
+    public_script = models.ForeignKey(
+        "public_scripts.PublicScript",
+        on_delete=models.CASCADE,
+        related_name="scripts",
+    )
+
+    number = models.PositiveSmallIntegerField(
+        verbose_name="스크립트 순번",
+        validators=[
+            MaxValueValidator(10, "최대 10개의 스크립트까지 가능합니다."),
+        ],
+    )
+
+    script = models.TextField(
+        verbose_name="스크립트",
+        help_text="ChatGPT에게 전달할 스크립트",
+    )
