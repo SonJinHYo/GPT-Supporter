fcc86db0365247fd7e360647153034da3de50d84
SonJinHYo <wlsgy95@naver.com>
2023-09-02 21:44:34 +0900

    backend-chore: 로컬 개발 설정 및 관리자 계정 생성코드 추가 local_build.sh - 환경변수에서 관리자 계정 정보를 불러와서 서버 시작 전 생성코드 추가 nginx.conf - docker-compose의 컨테이너 명으로 프록시 전달

--- backend/local_build.sh
+++ backend/local_build.sh
@@ -3,5 +3,24 @@
 # [local] 
 python3 manage.py makemigrations --settings=config.settings.local
 python3 manage.py migrate --settings=config.settings.local 
+
+USERNAME=$USERNAME
+PASSWORD=$PASSWORD
+EMAIL=$EMAIL
+
+# Django Superuser 생성
+python3 manage.py shell --settings=config.settings.local <<EOF
+from users.models import User
+ 
+# 이미 존재하는 사용자 확인
+user=User.objects.filter(username='$USERNAME')
+
+# 새로운 관리자 계정 생성
+if not user.exists():
+    User.objects.create_superuser('$USERNAME', '$EMAIL', '$PASSWORD')
+    print("관리자 계정 생성")
+
+EOF
+
 python3 manage.py runserver 0.0.0.0:8000 --settings=config.settings.local 
 # gunicorn -c gunicorn_conf.py config.wsgi.local:application
