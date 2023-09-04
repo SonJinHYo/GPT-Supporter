fe3980285b70e3c350b7fd83bd5fdf13e85ea2d4
SonJinHYo <wlsgy95@naver.com>
2023-09-04 13:13:28 +0900

    backend-feature: PublicScripts 리스트 데이터직렬화 수정 기존 PublicScripts 관련 모든 데이터를 보여주는 시리얼라이저는 Detail데이터로 사용. 리스트 뷰 전용 시리얼라이저는 pk,name,description만 보여주고 description은 최대 400자까지만 전송.

--- backend/public_scripts/serializers.py
+++ backend/public_scripts/serializers.py
@@ -20,12 +20,13 @@ class CreateScriptSerializer(ModelSerializer):
         )
 
 
-class PublicScriptsListSerializer(ModelSerializer):
+class PublicScriptDetail(ModelSerializer):
     scripts = SerializerMethodField()
 
     class Meta:
         model = PublicScript
         fields = (
+            "pk",
             "name",
             "description",
             "scripts",
@@ -33,3 +34,22 @@ class PublicScriptsListSerializer(ModelSerializer):
 
     def get_scripts(self, obj: PublicScript) -> list:
         return [script.text for script in obj.scripts.all().order_by("number")]
+
+
+class PublicScriptsListSerializer(ModelSerializer):
+    description_summary = SerializerMethodField()
+
+    class Meta:
+        model = PublicScript
+        fields = (
+            "pk",
+            "name",
+            "description_summary",
+        )
+
+    def get_description_summary(self, obj: PublicScript) -> list:
+        return (
+            obj.description
+            if len(obj.description) < 400
+            else obj.description[:400] + "..."
+        )
